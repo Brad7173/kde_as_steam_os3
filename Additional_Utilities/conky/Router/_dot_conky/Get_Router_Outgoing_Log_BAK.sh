e303f14964945d9118cc4925db63dc248c668ce4
Brad7173 <94210825+Brad7173@users.noreply.github.com>
2023-08-16 10:36:05 -0500

    Add files via upload

---
+++ Additional_Utilities/conky/Router/_dot_conky/Get_Router_Outgoing_Log_BAK.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+#~/.conky/Router/.conky/Get_Router_Outgoing_Log.sh
+#dt=$(date '+%D %T %p')
+clear
+RtrIP=$(ip route | grep default | awk '{print $3}')
+wget -qO- http://Admin:password@$RtrIP/Log_outgoing.asp > ~/.conky/Router/.conky/Router_Outgoing_Log_Raw.txt
+RtrOutgoingLogRaw=$(cat ~/.conky/Router/.conky/Router_Outgoing_Log_Raw.txt)
+echo $RtrOutgoingLogRaw | hxnormalize -x | w3m -dump -T 'text/html' | sed -r '/^\s*$/d'
+exit
