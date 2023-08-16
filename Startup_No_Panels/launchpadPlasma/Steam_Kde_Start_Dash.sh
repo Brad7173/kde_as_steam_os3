87d43dd2209c58857fef025fdb8dbfbd45a66c6f
Brad7173 <94210825+Brad7173@users.noreply.github.com>
2023-08-15 22:32:49 -0500

    Delete Startup_No_Panels/launchpadPlasma directory

--- Startup_No_Panels/launchpadPlasma/Steam_Kde_Start_Dash.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#By Katie M. Nelson 8/14/23
-#a script to open an app menu from steam with no desktop panels
-#KDE window rules need to be applied to maximize the window
-plasmawindowed launchpadPlasma
-exit
