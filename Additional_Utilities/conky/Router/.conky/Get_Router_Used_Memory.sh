5a3f1729835bb05645f72d97e9f4fc996dc360c6
Brad7173 <94210825+Brad7173@users.noreply.github.com>
2023-08-17 05:23:36 -0500

    Update Get_Router_Used_Memory.sh

--- Additional_Utilities/conky/Router/.conky/Get_Router_Used_Memory.sh
+++ Additional_Utilities/conky/Router/.conky/Get_Router_Used_Memory.sh
@@ -1,8 +1,8 @@
 #! /bin/sh
-#~/.conky/router/.conky/Get_Router_Used_Memory.sh
+#~/.conky/Router/.conky/Get_Router_Used_Memory.sh
 RtrIP=$(ip route | grep default | awk '{print $3}')
-wget -qO- http://Admin:yourpassword@$RtrIP/Status_Router.asp | grep 'setMemoryValues' | hxnormalize -x |grep -o '[0-9]*' > ~/.conky/router/.conky/Router_mem.txt
-RtrMemTotal=$(sed -n '10p' ~/.conky/router/.conky/Router_mem.txt)
-RtrMemUsed=$(sed -n '11p' ~/.conky/router/.conky/Router_mem.txt)
+wget -qO- http://Admin:yourpassword@$RtrIP/Status_Router.asp | grep 'setMemoryValues' | hxnormalize -x |grep -o '[0-9]*' > ~/.conky/Router/.conky/Router_mem.txt
+RtrMemTotal=$(sed -n '10p' ~/.conky/Router/.conky/Router_mem.txt)
+RtrMemUsed=$(sed -n '11p' ~/.conky/Router/.conky/Router_mem.txt)
 RtrAvailableMem=$(($RtrMemTotal - $RtrMemUsed))
 echo "$RtrMemTotal" "$RtrMemUsed" | awk '{print ($1-$2)/$1*100}'| awk '{print int($1+0.5)}'
