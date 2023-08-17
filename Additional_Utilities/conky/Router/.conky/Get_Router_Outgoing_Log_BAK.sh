f83cd8cdf2531cc75013d0a05e23af56942b5bc3
Brad7173 <94210825+Brad7173@users.noreply.github.com>
2023-08-17 05:22:57 -0500

    Update Get_Router_Outgoing_Log_BAK.sh

--- Additional_Utilities/conky/Router/.conky/Get_Router_Outgoing_Log_BAK.sh
+++ Additional_Utilities/conky/Router/.conky/Get_Router_Outgoing_Log_BAK.sh
@@ -1,9 +1,9 @@
 #! /bin/sh
-#~/.conky/router/.conky/Get_Router_Outgoing_Log.sh
+#~/.conky/Router/.conky/Get_Router_Outgoing_Log.sh
 #dt=$(date '+%D %T %p')
 clear
 RtrIP=$(ip route | grep default | awk '{print $3}')
-wget -qO- http://Admin:yourpassword@$RtrIP/Log_outgoing.asp > ~/.conky/router/.conky/Router_Outgoing_Log_Raw.txt
-RtrOutgoingLogRaw=$(cat ~/.conky/router/.conky/Router_Outgoing_Log_Raw.txt)
+wget -qO- http://Admin:yourpassword@$RtrIP/Log_outgoing.asp > ~/.conky/Router/.conky/Router_Outgoing_Log_Raw.txt
+RtrOutgoingLogRaw=$(cat ~/.conky/Router/.conky/Router_Outgoing_Log_Raw.txt)
 echo $RtrOutgoingLogRaw | hxnormalize -x | w3m -dump -T 'text/html' | sed -r '/^\s*$/d'
 exit
