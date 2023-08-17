85fe938984e4c8c5292c077adab0918c76e88023
Brad7173 <94210825+Brad7173@users.noreply.github.com>
2023-08-17 05:20:42 -0500

    Update Get_Router_Incoming_Log.sh

--- Additional_Utilities/conky/Router/.conky/Get_Router_Incoming_Log.sh
+++ Additional_Utilities/conky/Router/.conky/Get_Router_Incoming_Log.sh
@@ -1,11 +1,11 @@
 #! /bin/sh
-#~/.conky/router/.conky/Get_Router_Incoming_Log.sh
+#~/.conky/Router/.conky/Get_Router_Incoming_Log.sh
 Time=$(date +"%I:%M-%p")
 clear
 RtrIP=$(ip route | grep default | awk '{print $3}')
-wget -qO- http://Admin:password@$RtrIP/Log_incoming.asp > ~/.conky/router/.conky/Router_Incoming_Log_Raw.txt
-RtrIncomingLogRaw=$(cat ~/.conky/router/.conky/Router_Incoming_Log_Raw.txt)
+wget -qO- http://Admin:password@$RtrIP/Log_incoming.asp > ~/.conky/Router/.conky/Router_Incoming_Log_Raw.txt
+RtrIncomingLogRaw=$(cat ~/.conky/Router/.conky/Router_Incoming_Log_Raw.txt)
 printf '%s\n' $Time >> ~/Logs/$(date +%Y-%m-%d)-Router_Incoming.log
-cat ~/.conky/router.conky/Router_Incoming_Log_Raw.txt | hxnormalize -x | w3m -dump -T 'text/html' | sed -r '/^\s*$/d' >> ~/Logs/$(date +%Y-%m-%d)-Router_Incoming.log
+cat ~/.conky/Router.conky/Router_Incoming_Log_Raw.txt | hxnormalize -x | w3m -dump -T 'text/html' | sed -r '/^\s*$/d' >> ~/Logs/$(date +%Y-%m-%d)-Router_Incoming.log
 echo $RtrIncomingLogRaw | hxnormalize -x | w3m -dump -T 'text/html' | sed -r '/^\s*$/d'
 exit
